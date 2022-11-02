4bdb4d13daf6c1ba862baa402bfe9dc8f4b09fcd
sedehi <sedehi@users.noreply.github.com>
2022-11-02 12:15:29 +0000

    Fix styling

--- src/LaravelModuleServiceProvider.php
+++ src/LaravelModuleServiceProvider.php
@@ -6,13 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelModuleServiceProvider extends ServiceProvider
 {
-
-    public function boot(){
+    public function boot()
+    {
         view()->addLocation(app_path('Modules'));
-        foreach (glob(app_path('Modules/*/views')) as $modulePath){
-            $module = explode('/',$modulePath);
-            $module = $module[count($module)-2];
-            view()->addNamespace($module,app_path('Modules/'.$module.'/views'));
+        foreach (glob(app_path('Modules/*/views')) as $modulePath) {
+            $module = explode('/', $modulePath);
+            $module = $module[count($module) - 2];
+            view()->addNamespace($module, app_path('Modules/'.$module.'/views'));
         }
     }
 
