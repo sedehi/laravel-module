ea443a9312688766baf14977df7d65a4f492a0e1
sedehi <sedehi@users.noreply.github.com>
2022-11-02 10:56:06 +0000

    Fix styling

--- src/LaravelModuleServiceProvider.php
+++ src/LaravelModuleServiceProvider.php
@@ -4,12 +4,11 @@ namespace Sedehi\LaravelModule;
 
 use Illuminate\Support\ServiceProvider;
 
-
 class LaravelModuleServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        if($this->app->runningInConsole()){
+        if ($this->app->runningInConsole()) {
             $this->app->register(CommandsServiceProvider::class);
         }
     }
