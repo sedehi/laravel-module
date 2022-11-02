5e149dcad90d893046b58f82b9f2ac71fbcc19ce
Navid Sedehi <navid.sedehi@gmail.com>
2022-11-02 14:23:41 +0330

    make job as an action

--- src/Commands/MakeJob.php
+++ src/Commands/MakeJob.php
@@ -7,13 +7,31 @@ use Sedehi\LaravelModule\Commands\Questions\ModuleName;
 use Sedehi\LaravelModule\Traits\CommandOptions;
 use Sedehi\LaravelModule\Traits\Interactive;
 use Sedehi\LaravelModule\Traits\ModuleNameOption;
+use Symfony\Component\Console\Input\InputOption;
 
 class MakeJob extends JobMakeCommand implements ModuleName
 {
-    use CommandOptions,Interactive,ModuleNameOption;
+    use Interactive,ModuleNameOption;
 
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace.$this->getModuleName().'\Jobs';
+        $rootNamespace = $rootNamespace.$this->getModuleName();
+        if($this->option('action')){
+            return $rootNamespace.'\Actions';
+        }
+        return $rootNamespace.'\Jobs';
+    }
+
+
+    protected function getOptions()
+    {
+        $options = parent::getOptions();
+        $options = array_merge($options, [
+            ['action', 'aa', InputOption::VALUE_NONE, 'Generate as an action'],
+            ['module', null, InputOption::VALUE_OPTIONAL, 'The name of the module'],
+            ['in', false, InputOption::VALUE_NONE, 'Interactive mode'],
+        ]);
+
+        return $options;
     }
 }
--- tests/Commands/MakeJobTest.php
+++ tests/Commands/MakeJobTest.php
@@ -24,6 +24,22 @@ class MakeJobTest extends TestCase
      * @return void
      * @test
      */
+    public function it_can_make_a_new_job_class_as_an_action()
+    {
+        $this->artisan('make:job', [
+            'name' => $this->sampleName,
+            '--action' => true,
+            '--module' => $this->moduleName,
+        ]);
+
+        $this->assertFileExists(app_path('Modules/'.$this->moduleName."/Actions/{$this->sampleName}.php"));
+
+    }
+
+    /**
+     * @return void
+     * @test
+     */
     public function it_can_make_job_class_interactive()
     {
         $this->artisan('make:job', [
