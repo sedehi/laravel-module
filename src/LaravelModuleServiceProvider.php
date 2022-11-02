4233776af23243f19a7565039769f81cb99e4eef
Navid Sedehi <navid.sedehi@gmail.com>
2022-11-02 15:46:34 +0330

    view paths

--- src/LaravelModuleServiceProvider.php
+++ src/LaravelModuleServiceProvider.php
@@ -8,18 +8,26 @@ class LaravelModuleServiceProvider extends ServiceProvider
 {
 
     public function boot(){
-        view()->addLocation(app_path('Modules'));
-        foreach (glob(app_path('Modules/*/views')) as $modulePath){
-            $module = explode('/',$modulePath);
-            $module = $module[count($module)-2];
-            view()->addNamespace($module,app_path('Modules/'.$module.'/views'));
-        }
+        $this->loadViews();
     }
 
     public function register()
     {
         if ($this->app->runningInConsole()) {
             $this->app->register(CommandsServiceProvider::class);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function loadViews(): void
+    {
+        view()->addLocation(app_path('Modules'));
+        foreach (glob(app_path('Modules/*/views')) as $modulePath) {
+            $module = explode('/', $modulePath);
+            $module = $module[count($module) - 2];
+            view()->addNamespace($module, app_path('Modules/' . $module . '/views'));
         }
     }
 }
