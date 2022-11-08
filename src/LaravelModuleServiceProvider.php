caa6255d8125c3379b08beee97f0d88a1b6ff5fa
Navid Sedehi <navid.sedehi@gmail.com>
2022-11-08 13:20:05 +0330

    wip

--- src/LaravelModuleServiceProvider.php
+++ src/LaravelModuleServiceProvider.php
@@ -3,12 +3,14 @@
 namespace Sedehi\LaravelModule;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class LaravelModuleServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadViews();
+        $this->viewComposers();
     }
 
     public function register()
@@ -29,5 +31,19 @@ class LaravelModuleServiceProvider extends ServiceProvider
             $module = $module[count($module) - 2];
             view()->addNamespace($module, app_path('Modules/'.$module.'/views'));
         }
+    }
+
+    private function viewComposers(){
+        view()->composer('*::admin.*.*',function ($view){
+            $str = Str::of(request()->route()->getActionName())->after('App\Modules\\')->explode('\\');
+            $module = $str->first();
+            $controllerWithMethod = explode('@',$str->last());
+            $controller = head($controllerWithMethod);
+            $method = end($controllerWithMethod);
+            if($controller == $method){
+                $method = '';
+            }
+            return $view->with('module', $module)->with('controller',$controller)->with('method',$method);
+        });
     }
 }
