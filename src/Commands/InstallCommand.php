eea4f4a45755e4f586d17261ba812f9e60f31276
Navid Sedehi <navid.sedehi@gmail.com>
2023-01-20 19:57:37 +0330

    wip

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -3,6 +3,7 @@
 namespace Sedehi\LaravelModule\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 
 class InstallCommand extends Command
@@ -46,6 +47,33 @@ class InstallCommand extends Command
             return $string->beforeLast('}')->append(file_get_contents(__DIR__.'/stubs/install/route-web-method.stub').'}')
                 ->replace('$this->routes(function () {', PHP_EOL.'$this->routes(function () {'.PHP_EOL.'$this->mapModuleWebRoutes();');
         });
+
         file_put_contents($providerPath, $str);
+        Artisan::call('make:provider',['name' => 'ModuleServiceProvider']);
+        $this->registerModuleServiceProvider();
     }
+
+
+    /**
+     * Register the Horizon service provider in the application configuration file.
+     *
+     * @return void
+     */
+    protected function registerModuleServiceProvider()
+    {
+        $namespace = Str::replaceLast('\\', '', $this->laravel->getNamespace());
+
+        $appConfig = file_get_contents(config_path('app.php'));
+
+        if (Str::contains($appConfig, $namespace.'\\Providers\\ModuleServiceProvider::class')) {
+            return;
+        }
+
+        file_put_contents(config_path('app.php'), str_replace(
+            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL,
+            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL."        {$namespace}\Providers\ModuleServiceProvider::class,".PHP_EOL,
+            $appConfig
+        ));
+    }
+
 }
